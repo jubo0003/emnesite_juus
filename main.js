85063494daed410bd2886248a1f8917e4b844cee
Julie Bosch <jubo0003@stud.ek.dk>
2025-10-21 23:22:37 +0200

    added dropdown menu + burger

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 const summaries = document.querySelector(".summaries1");
 
-summaries.addEventListener("click", (e) => {
-  console.log(e.target);
-  const allDetails = Array.from(e.currentTarget.querySelectorAll("details"));
-  const clickedDetails = e.target.closest("details");
-  allDetails
-    .filter((detail) => detail !== clickedDetails)
-    .forEach((remainigDetail) => {
-      remainigDetail.removeAttribute("open");
-    });
-});
+if (summaries) {
+  summaries.addEventListener("click", (e) => {
+    console.log(e.target);
+    const allDetails = Array.from(e.currentTarget.querySelectorAll("details"));
+    const clickedDetails = e.target.closest("details");
+    allDetails
+      .filter((detail) => detail !== clickedDetails)
+      .forEach((remainigDetail) => {
+        remainigDetail.removeAttribute("open");
+      });
+  });
+}
 
-let menuBurger = document.querySelector(".burger");
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
 
-menuBurger.addEventListener("click", function (event) {
-  // event.target = hvad der er blevet klikket på
-  // event.currentTarget = hvad event listeneren original er bundet op på (.burger)
-});
+burger.addEventListener("click", burgerClick);
+
+function burgerClick() {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+const submenuButton = document.querySelector(".submenu-button");
+const submenus = document.querySelector(".submenus");
+if (submenuButton) {
+  submenuButton.addEventListener("click", (e) => {
+    submenus.classList.toggle("active");
+  });
+}
